2d21c99145a4460b0cb49300f20de3a1942d34f1
Kumirena <irshel1@ya.ru>
2023-10-24 07:10:09 +0300

    Seminar_4

---
+++ TestUnit/src/main/java/HWSeminar4/BookRepository.java
@@ -0,0 +1,8 @@
+package HWSeminar4;
+
+import java.util.List;
+
+public interface BookRepository {
+    Book findById(String id);
+    List<Book> findAll();
+}
---
+++ TestUnit/src/test/java/Seminar4/DummyPaymentGateway.java
@@ -0,0 +1,10 @@
+package Seminar4;
+
+public class DummyPaymentGateway implements PaymentGateway {
+
+    @Override
+    public boolean processPayment(double amount){
+        return true;
+        // Не выполняем никаких действий, просто фиктивный объект return true;
+    }
+}
